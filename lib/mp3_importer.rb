1098cd55b8591d54d0ed8386691e8dd250218af5
Sarah Williams <srwilliams8678@gmail.com>
2019-05-29 17:42:00 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,7 +1,21 @@
 class MP3Importer
-  
-  
-  def import(list_of_filenames)
-    list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    files = []
+    Dir.new(self.path).each do |file|
+      files << file if file.length > 4
+    end
+    files
+  end
+
+  def import
+    self.files.each do |filename|
+      Song.new_by_filename(filename)
+    end
   end
 end

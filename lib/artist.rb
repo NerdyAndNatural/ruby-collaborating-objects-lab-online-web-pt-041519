4da1eb32dd788bc41ee805babbbe6302a6d71db2
Sarah Williams <srwilliams8678@gmail.com>
2019-05-29 17:50:39 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,4 +1,28 @@
 class Artist
+  class Artist
+  attr_accessor :song
+
+  def initialize(song, title)
+    @song = song
+    @title = title
+  end
+
+  def add_song ()
+    files = []
+    Dir.new(self.path).each do |file|
+      files << file if file.length > 4
+    end
+    files
+  end
+
+  def import
+    self.files.each do |filename|
+      Song.new_by_filename(filename)
+    end
+  end
+end
+
+  
   
   
 end
